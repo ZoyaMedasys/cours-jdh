5c1a0fe0f871f941a0f41ba2ecd7bd7c36178e95
Administrateur <Administrateur@192.168.14.6>
2015-07-01 15:55:06 +0200

    DAY 3

--- mytp/lib/commande.js
+++ mytp/lib/commande.js
@@ -1,37 +1,24 @@
 	
-	/**
-	 * Verifie l'information d'une adresse IP
-	*/
-	function verifieIp()
+function init()
 {
-     expReg = /^((([01]?[0-9]?[0-9])|(2[0-4][0-9])|(25[0 -5]))[.]){3}(([0-1]?[0-9]?[0-9])|(2[0-4][0-9])|(25[0 -5])){1}$/
-     leip = document.forms[0].ip.value;
-     tab = leip.search(expReg);
-     if (tab!=-1)
-          result = "Adresse IP valide";
-     else result = "Adresse IP fausse";
-     document.forms[0].result.value = result;
+    var l_textsource = document.getElementById("textsource").value;
+    console.log(l_textsource);
+    document.getElementById("textcopy").innerHTML=l_textsource;
+   
 }
 
-	
-	
-	
-	
-	
-	function met_en(quoi)
-	{
-	     var deja = false ;
-	     if (window.getSelections)
-	        txt = window.getSelection();
-	     else if (document.getSelection)
-	       txt = document.getSelection() ;
-	     else if (document.selection)
-	       txt = document.selection.createRange().text;
-	     init(document.formu.texte.value);
-	}
-	
-	
-	function init(txt)
-	{
-	     document.getElementById("voirtxt").innerHTML = txt.replace( /\n/g,"<br/>")
-	}
+function copytext()
+{
+    var l_textsource = document.getElementById("textsource").value;
+    console.log(l_textsource);
+    document.getElementById("textcopy").innerHTML=l_textsource;
+   
+}
+
+function copytext(event)
+{
+    document.getElementById("textcopy").innerHTML = event.target.value;
+}
+
+window.addEventListener('select',copytextonselect(event) );
+window.addEventListener('click',copytext(event) );
